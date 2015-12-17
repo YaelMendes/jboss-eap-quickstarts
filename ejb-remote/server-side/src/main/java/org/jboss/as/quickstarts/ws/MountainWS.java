1dc112ba110ccfa5dedabce5a70a375e9bb57b33
Yaël Mendes <yael.mendes-ext@pole-emploi.fr>
2015-12-17 16:56:02 +0100

    soap not ok

---
+++ ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ws/MountainWS.java
@@ -0,0 +1,7 @@
+package org.jboss.as.quickstarts.ws;
+
+/**
+ * Created by EYME5300 on 17/12/15.
+ */
+public interface MountainWS {
+}
---
+++ ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ws/impl/MountainWSImpl.java
@@ -0,0 +1,7 @@
+package org.jboss.as.quickstarts.ws.impl;
+
+/**
+ * Created by EYME5300 on 17/12/15.
+ */
+public class MountainWSImpl {
+}
